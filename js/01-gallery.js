2658d6f0fc9ee79f350370a9bd4ff586d3c05ec6
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-11-30 16:16:59 +0200

    save 1 home

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,48 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`);
+const listRef = document.querySelector(".gallery");
+const itemsList = galleryItems
+  .map(
+    (element) => `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+          <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+          />
+        </a>
+      </li>`
+  )
+  .join("");
+listRef.innerHTML = itemsList;
 
-instance.show();
-console.log(galleryItems);
+const instance = basicLightbox.create(
+  `
+		<img width="1400" height="900" src="">
+	`,
+  {
+    onShow: () => {
+      document.addEventListener("keydown", keyBordPress);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", keyBordPress);
+    },
+  }
+);
+// instance.show();
+
+listRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance.element().querySelector("img").src = event.target.dataset.source;
+
+  instance.show();
+});
+function keyBordPress(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+}
