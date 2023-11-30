3c77e10745a3e28d94d11eb47900a09469bb90e4
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-11-30 17:20:36 +0200

    save help

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const listRef = document.querySelector(".gallery");
+const itemsList = galleryItems
+  .map(
+    (element) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+          <img
+            class="gallery__image"
+            src="${element.preview}"
+            alt="${element.description}"
+          />
+          <div class="gallery__caption">${element.description}</div>
+        </a>
+      </li>`
+  )
+  .join("");
+listRef.innerHTML = itemsList;
+
+const scriptRef = document.createElement("script");
+scriptRef.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.7.0/simple-lightbox.min.js";
+document.head.appendChild(scriptRef);
+
+const linkRef = document.createElement("link");
+linkRef.rel = "stylesheet";
+linkRef.href =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.7.0/simple-lightbox.min.css";
+document.head.appendChild(linkRef);
+
+scriptRef.onload = () => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+};
